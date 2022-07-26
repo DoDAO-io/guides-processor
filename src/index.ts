6ddca731bf455715bf25bc79c60d7af0b262b5f8
robin <robinnagpal.tiet@gmail.com>
2022-07-26 14:25:54 -0400

    Add another validation task

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { validateUniqueUUIDs } from './validation/validateUniqueUUIDs';
 
 const program = new Command();
 
-export function validateFiles(srcPath: string) {
+export function validateFilesAndGenerateFiles(srcPath: string) {
   const courseDirPath = process.cwd() + '/' + srcPath;
   console.log('Validating Course...');
   validateCourseFile(`${courseDirPath}/course.yaml`);
@@ -26,6 +26,21 @@ export function validateFiles(srcPath: string) {
   generateCourseFiles(courseDirPath);
 }
 
+export function validateFiles(srcPath: string) {
+  const courseDirPath = process.cwd() + '/' + srcPath;
+  console.log('Validating Course...');
+  validateCourseFile(`${courseDirPath}/course.yaml`);
+  validateSummariesFiles(`${courseDirPath}/summaries`);
+  validateQuestionFiles(`${courseDirPath}/questions`);
+  validateReadingsFiles(`${courseDirPath}/readings`);
+  validateUniqueUUIDs(
+    `${courseDirPath}/summaries`,
+    `${courseDirPath}/readings`,
+    `${courseDirPath}/questions`
+  );
+  console.log('Congrats! Everything looks good!');
+}
+
 program
   .name('course-processor')
   .description('CLI to for generating metadata for DoDAO courses')
@@ -34,6 +49,14 @@ program
 program
   .command('gen-all')
   .description('Generates both the markdown and JSON metadata for the course')
+  .argument('<path>', 'path of the course folder')
+  .action(async path => {
+    validateFilesAndGenerateFiles(path);
+  });
+
+program
+  .command('validate')
+  .description('Validates the course files')
   .argument('<path>', 'path of the course folder')
   .action(async path => {
     validateFiles(path);
--- src/validation/validateQuestionFiles.ts
+++ src/validation/validateQuestionFiles.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { Validator } from 'jsonschema';
+import { Question } from '../model/Question';
 import { throwValidationError } from './throwValidationError';
 import { readFilesSync } from '../utils/readFilesSync';
 import YAML from 'yaml';
@@ -9,11 +10,19 @@ export function validateQuestionFiles(questionDirectory: string) {
   const questionFiles = readFilesSync(questionDirectory);
   questionFiles.forEach(questionFile => {
     const file = fs.readFileSync(questionFile.filepath, 'utf8');
-    const questionsJson = YAML.parse(file);
+    const questionsJson = YAML.parse(file) as Question[];
     const v = new Validator();
     const res = v.validate(questionsJson, questionsFileSchema);
     if (!res.valid) {
       throwValidationError(questionFile.filepath, res.errors);
     }
+
+    questionsJson.forEach(question => {
+      if (!Array.isArray(question.answerKeys)) {
+        throw Error(
+          `Answer Keys of question: ${question.content} is not an array`
+        );
+      }
+    });
   });
 }
