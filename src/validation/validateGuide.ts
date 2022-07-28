f2e75cc5a0e0e6bc1e4ef320db7818ced2ca0ec8
robin <robinnagpal.tiet@gmail.com>
2022-07-27 21:30:28 -0400

    Initial commit

--- src/validation/validateGuide.ts
+++ src/validation/validateGuide.ts
@@ -5,6 +5,9 @@ import { Validator } from 'jsonschema';
 import guideFileSchema from './schemas/guideSchema.json';
 import guideIntegrationsSchema from './schemas/guideIntegrationsSchema.json';
 import guideStepSchema from './schemas/guideStepSchema.json';
+import discordConnectSchema from './schemas/discordConnectSchema.json';
+import userInputSchema from './schemas/userInputSchema.json';
+import questionSchema from './schemas/questionSchema.json';
 
 export function validateGuide(guideFilePath: string) {
   const file = fs.readFileSync(guideFilePath, 'utf8');
@@ -12,6 +15,9 @@ export function validateGuide(guideFilePath: string) {
   const v = new Validator();
   v.addSchema(guideIntegrationsSchema, '/GuideIntegrationsSchema');
   v.addSchema(guideStepSchema, '/GuideStepSchema');
+  v.addSchema(discordConnectSchema, '/DiscordConnectSchema');
+  v.addSchema(userInputSchema, '/UserInputSchema');
+  v.addSchema(questionSchema, '/QuestionSchema');
 
   const res = v.validate(guideJson, guideFileSchema);
   if (!res.valid) {
