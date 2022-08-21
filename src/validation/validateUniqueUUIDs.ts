49f196f11607ac13efa5f548e28fa11067f8b764
robin <robinnagpal.tiet@gmail.com>
2022-08-20 20:50:57 -0400

    Add more details

--- src/validation/validateUniqueUUIDs.ts
+++ src/validation/validateUniqueUUIDs.ts
@@ -10,21 +10,27 @@ export function validateUniqueUUIDs(srcDirPath: string, guidesJson: string[]) {
     const file = fs.readFileSync(guideFilePath, 'utf8');
     const guideJson = YAML.parse(file) as GitGuideModel;
     if (uuids.includes(guideJson.uuid)) {
-      throw new Error(`Duplicate Guide UUID ${guideJson.uuid}`);
+      throw new Error(`Duplicate Guide UUID ${guideJson.uuid} in ${guide}`);
     } else {
       uuids.push(guideJson.uuid);
     }
 
     guideJson.steps.forEach(step => {
       if (uuids.includes(step.uuid)) {
-        throw new Error(`Duplicate Step UUID ${step.uuid}`);
+        throw new Error(`Duplicate Step UUID ${step.uuid} in ${guide}`);
       } else {
         uuids.push(step.uuid);
       }
 
+      if (!step.stepItems) {
+        throw new Error(`No Step Item present for ${step.name} in ${guide}`);
+      }
+
       step.stepItems.forEach(stepItem => {
         if (uuids.includes(stepItem.uuid)) {
-          throw new Error(`Duplicate Step Item UUID ${stepItem.uuid}`);
+          throw new Error(
+            `Duplicate Step Item UUID ${stepItem.uuid} in ${guide}`
+          );
         } else {
           uuids.push(stepItem.uuid);
         }
